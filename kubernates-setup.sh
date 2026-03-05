04b6ce04ebc8f858d75b133f37c5e650184e1464
Joaquin Ruiz <joaquin.ruiz7rp@gmail.com>
2026-03-05 12:57:04 +0100

    Kubernates orquestration automated with shel script

---
+++ kubernates-setup.sh
@@ -0,0 +1,48 @@
+docker compose build
+
+echo "Fetch your RAWG api key from https://rawg.io/apidocs"
+
+while [ -z "$RAWG_API_KEY" ]; do
+  read -p "Enter your RAWG API key: " RAWG_API_KEY
+done
+
+kubectl create secret generic videogames-backend-service-secrets \
+  --from-literal=RAWG_API_KEY="$RAWG_API_KEY" \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+#Create backend pods
+kubectl apply -f ./videogames-backend/kubernates/deploy/manifest.yaml
+#Create backend services
+kubectl apply -f ./videogames-backend/kubernates/services/manifest.yaml
+
+#Create frontend config map
+kubectl apply -f ./videogames-frontend/kubernates/config-map/manifest.yaml
+
+#Create frontend pods
+kubectl apply -f ./videogames-frontend/kubernates/deploy/manifest.yaml
+#Create frontend service
+kubectl apply -f ./videogames-frontend/kubernates/service/manifest.yaml
+#Create frontend ingress
+kubectl apply -f ./videogames-frontend/kubernates/ingress/manifest.yaml
+
+echo "Waiting for ingress IP..."
+
+INGRESS_IP=$(kubectl get ingress videogames-ingress -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+echo "Ingress IP: $INGRESS_IP"
+
+HOST="videogames.local"
+
+if grep -q "$HOST" /etc/hosts; then
+  echo "$HOST already exists in /etc/hosts"
+else
+  echo "Adding $HOST → $INGRESS_IP to /etc/hosts"
+  echo "$INGRESS_IP $HOST" | sudo tee -a /etc/hosts > /dev/null
+fi
+
+echo ""
+echo "Application available at:"
+echo "http://$HOST"
+
+
+
